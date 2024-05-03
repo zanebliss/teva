aa41068feb9b48a963d2fbc2835c088171e2d3b2
Zane Bliss <zanebliss@icloud.com>
2024-05-03 15:14:22 -0500

    display gitavs logo on process start
    Cute? Maybe. Fun? yes. Subject to being removed.
    
    I could see this being "more" useful when the command when/if `gitavs` runs
    in the background.

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,24 @@ struct Cli {
     from_sha: Option<String>,
 }
 
+fn print_logo() {
+    println!(
+        r"
+      (_)  _
+  ____ _ _| |_ _____ _   _ ___
+ / _  | (_   _|____ | | | /___)
+( (_| | | | |_/ ___ |\ V /___ |
+ \___ |_|  \__)_____| \_/(___/
+(_____|
+             "
+    )
+}
+
 fn main() {
     let cached_files: Vec<String> = vec![];
     let cli = Cli::parse();
 
+    print_logo();
 
     git_client::do_work(
         String::from(cli.from_sha.as_deref().unwrap_or("main")),
