8fa9ca540e82692c30ed3f13dce6ca060d0878f9
Zane Bliss <zanebliss@icloud.com>
2024-05-08 22:37:13 -0500

    add `runners::node`
    This creates a `runners::node` module which has a `setup_environment()`
    function. Although running node tests are not yet supported, there are
    some rails apps where sprockets will attempt to access `node_modules`,
    which will fail when running ruby tests.
    
    Instead of installing dependencies in the worktree directory, this
    instead creates a symlink from the repo path and the worktree directory.
    It also runs `yarn`. While I would like to provide a runner for `npm`,
    the project this is being tested against uses `yarn`.

--- src/runners.rs
+++ src/runners.rs
@@ -33,3 +33,43 @@ pub mod ruby {
         }
     }
 }
+
+pub mod node {
+    use std::{
+        env::current_dir,
+        os::unix::fs::symlink,
+        path::PathBuf,
+        process::{self, Command},
+    };
+
+    const NODE_MODULES: &str = "node_modules";
+    const YARN: &str = "yarn";
+    pub const PACKAGE_JSON: &str = "package.json";
+
+    pub fn setup_environment(repo_dir: PathBuf) {
+        symlink_node_modules(repo_dir);
+
+        _ = match Command::new(YARN).output() {
+            Ok(_) => (),
+            Err(err) => {
+                eprintln!("Failed to run npm: {}", err);
+                process::exit(1);
+            }
+        }
+    }
+
+    fn symlink_node_modules(repo_dir: PathBuf) {
+        let current_dir = match current_dir() {
+            Ok(dir) => dir,
+            Err(err) => {
+                eprintln!("Error getting current_dir: {}", err);
+                process::exit(1)
+            }
+        };
+
+        _ = symlink(
+            format!("{}/{NODE_MODULES}", repo_dir.display()),
+            format!("{}/{NODE_MODULES}", current_dir.display()),
+        )
+    }
+}
