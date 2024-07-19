007be64e467c36d7c1214d7574f5379fead5c259
Zane Bliss <zanebliss@icloud.com>
2024-07-19 12:41:04 -0500

    add `display::Color` enum and `Display` trait
    This is a way to represent colors when writing output. The trait
    interpolates the color code when used in formatted strings.

---
+++ src/display.rs
@@ -0,0 +1,21 @@
+use std::fmt::Display;
+
+pub enum Color {
+    Blue,
+    Yellow,
+    Red,
+    None,
+}
+
+impl Display for Color {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let code = match &self {
+            Color::Blue => 94,
+            Color::Yellow => 33,
+            Color::Red => 91,
+            Color::None => 0,
+        };
+
+        write!(f, "{code}")
+    }
+}
