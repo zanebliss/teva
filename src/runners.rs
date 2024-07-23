3676b5aa31052d7785fa6f5782b3fc1beed4f7fe
Zane Bliss <zanebliss@icloud.com>
2024-07-22 18:24:19 -0700

    create `Runner` struct and `Ruby::Rspec` enum
    Upcoming changes will require supporting new types of runners such as
    `bin/rails test`. Refactoring this to a struct will allow storing the
    desired runner and persist it throughout the life-cycle of the program.

--- src/runners.rs
+++ src/runners.rs
@@ -1,3 +1,79 @@
+use std::{
+    io::Error,
+    path::{Path, PathBuf},
+    process::{Command, Stdio},
+};
+
+pub struct Runner {
+    pub runnable: Ruby,
+}
+
+pub enum Ruby {
+    Rspec,
+}
+
+impl Ruby {
+    fn program(&self) -> &str {
+        match &self {
+            Ruby::Rspec => "bundle",
+        }
+    }
+
+    fn args(&self) -> Vec<&str> {
+        match &self {
+            Ruby::Rspec => vec!["exec", "rspec"],
+        }
+    }
+
+    fn file_suffix(&self) -> &str {
+        match &self {
+            Ruby::Rspec => "_spec.rb",
+        }
+    }
+}
+
+impl Runner {
+    // Not every rails app will need this, but if sprockets attempts
+    // to load JS assets in a test it will fail
+    pub fn setup_environment(&self, repo_dir: PathBuf) -> Result<(), Error> {
+        if !repo_dir.join(node::PACKAGE_JSON).exists() {
+            return Ok(());
+        }
+
+        node::setup_environment(repo_dir)?;
+
+        Ok(())
+    }
+
+    pub fn run(&self, cached_files: &Vec<String>) -> Result<(), Error> {
+        let runnable_files = cached_files
+            .iter()
+            .map(|file| file.clone())
+            .filter(|file| Path::new(file).exists() && file.ends_with(self.runnable.file_suffix()))
+            .collect::<Vec<String>>();
+
+        if runnable_files.is_empty() {
+            return Ok(());
+        }
+
+        self.execute(runnable_files)?;
+
+        Ok(())
+    }
+
+    fn execute(&self, runnable_files: Vec<String>) -> Result<(), Error> {
+        Command::new(self.runnable.program())
+            .args(self.runnable.args())
+            .args(runnable_files)
+            .stderr(Stdio::null())
+            .spawn()
+            .unwrap()
+            .wait_with_output()?;
+
+        Ok(())
+    }
+}
+
 pub mod ruby {
     const BUNDLE: &str = "bundle";
     const EXEC: &str = "exec";
